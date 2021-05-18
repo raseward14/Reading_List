fd839dcb376115115b049ee2fbe6639f8ee7d3cb
Richard Seward <raseward14@gmail.com>
2021-05-17 21:01:41 -0600

    server updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://raseward14:Biscuit10MA!@cluster0.zfht0.mongodb.net/reactreadinglist?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
