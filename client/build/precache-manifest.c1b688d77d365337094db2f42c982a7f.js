06ce78e76d7a8aca4e2d5f4affb22bdbca140746
Richard Seward <raseward14@gmail.com>
2021-05-17 21:39:41 -0600

    added build folder

---
+++ client/build/precache-manifest.c1b688d77d365337094db2f42c982a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e684c3f0329622341e97138c7be84714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c82e5cc860c49ba8b70",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "563812fdcfa95d36e049",
+    "url": "/static/js/2.dbbf1802.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.dbbf1802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c82e5cc860c49ba8b70",
+    "url": "/static/js/main.bb0e859e.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
